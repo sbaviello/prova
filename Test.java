2694106c763abae41a5436e28962b112f9388d4f
Andrea Demontis <andreademontis.jobs@gmail.com>
2022-10-03 17:41:36 +0200

    Commit da Andrea

--- Test.java
+++ Test.java
@@ -2,5 +2,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("ciaooooo");
         System.out.println("ciao da emilia");
+        System.out.println("ciao da emilia 2");
     }
 }
